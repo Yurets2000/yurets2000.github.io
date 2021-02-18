ac186fc138f39634df6b983626254b1178c8d3b2
Yurii Bezliudnyi <yurii.bezliudnyi@netcracker.com>
2021-02-18 15:30:56 +0200

    Shopping Cart and Wish List pages added

---
+++ Lab1/js/main.js
@@ -0,0 +1,8 @@
+function openClose(id) {
+  let x = document.getElementById(id);
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
